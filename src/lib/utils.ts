1c27002c22f9007b76b53d0ff591558d841d6ae8
lucassfreiree <lucaslima4132@gmail.com>
2026-03-28 21:03:12 +0000

    Edited Spark

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,19 @@
 import { clsx, type ClassValue } from "clsx"
 import { formatDistanceToNow, format } from "date-fns"
-import { toZonedTime } from "date-fns-tz"
 import { ptBR } from "date-fns/locale"
-
-export function cn(...inputs: ClassValue[]) {
   return clsx(inputs)
-}
 
-export function formatDistanceToNowSaoPaulo(date: Date | string, options?: { addSuffix?: boolean }) {
-  const dateObj = typeof date === 'string' ? new Date(date) : date
-  const saoPauloTime = toZonedTime(dateObj, 'America/Sao_Paulo')
-  return formatDistanceToNow(saoPauloTime, { locale: ptBR, ...options })
-}
+  const dateObj = typeof date === 'string' ? 
+  return formatDistan
 
-export function formatSaoPauloTime(date: Date | string, formatStr: string = 'PPpp') {
-  const dateObj = typeof date === 'string' ? new Date(date) : date
-  const saoPauloTime = toZonedTime(dateObj, 'America/Sao_Paulo')
+
   return format(saoPauloTime, formatStr)
-}
+
+
+
+
+
+
+
+
+
