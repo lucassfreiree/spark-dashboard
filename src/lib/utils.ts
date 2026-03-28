7e20abdbcbe90e8b26685e4943163e2284257c56
lucassfreiree <lucaslima4132@gmail.com>
2026-03-28 21:00:21 +0000

    Generated by Spark: Fix all reported errors.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-
+import { formatDistanceToNow, format } from "date-fns"
+import { toZonedTime } from "date-fns-tz"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -9,6 +10,12 @@ export function cn(...inputs: ClassValue[]) {
 export function formatDistanceToNowSaoPaulo(date: Date | string, options?: { addSuffix?: boolean }) {
   const dateObj = typeof date === 'string' ? new Date(date) : date
   return formatDistanceToNow(dateObj, options)
+}
+
+export function formatSaoPauloTime(date: Date | string, formatStr: string = 'PPpp') {
+  const dateObj = typeof date === 'string' ? new Date(date) : date
+  const saoPauloTime = toZonedTime(dateObj, 'America/Sao_Paulo')
+  return format(saoPauloTime, formatStr)
 }
 
 
