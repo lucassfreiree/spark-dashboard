7b32737308121a09293a16c913e46aa87b2d7a1f
lucassfreiree <lucaslima4132@gmail.com>
2026-03-28 20:43:31 +0000

    Edited Spark

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,19 @@
 import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { toZonedTime } from 'date-fns-tz'
-import { formatDistanceToNow, format as dateFnsFormat } from 'date-fns'
+import { toZonedTime } from 'date-fns-tz
+import { ptBR } from 'date-fns/locale'
+export function cn(...inputs: ClassValue[]) {
 import { ptBR } from 'date-fns/locale'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+ 
+
+
+  const dateObj = typeof date === 'string' ? new Date(date) : date
+  const spTime = toZonedTime(dateObj, saoPaulo
 }
 
-export function formatDistanceToNowSaoPaulo(date: Date | string, options?: { addSuffix?: boolean }) {
-  const dateObj = typeof date === 'string' ? new Date(date) : date
-  const saoPauloTimeZone = 'America/Sao_Paulo'
-  const spTime = toZonedTime(dateObj, saoPauloTimeZone)
-  return formatDistanceToNow(spTime, { ...options, locale: ptBR })
-}
+
 
 export function formatSaoPauloTime(date: Date | string, formatStr: string = 'PPpp') {
   const dateObj = typeof date === 'string' ? new Date(date) : date
@@ -21,11 +21,6 @@ export function formatSaoPauloTime(date: Date | string, formatStr: string = 'PPp
   const spTime = toZonedTime(dateObj, saoPauloTimeZone)
   return dateFnsFormat(spTime, formatStr, { locale: ptBR })
 }
-
-
-
-
-
 
 
 
