55de78999a8db70c53144f23450ef6ff6ba5ad8d
lucassfreiree <lucaslima4132@gmail.com>
2026-03-28 20:50:33 +0000

    Edited Spark

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { formatDistanceToNow, format } from "date-fns"
 import { toZonedTime } from "date-fns-tz"
-import { ptBR } from "date-fns/locale"
 
-export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+
+  const saoPaulo = 'America/Sao_Paulo'
+  const spTime = toZonedTime(d
 }
 
 export function formatDistanceToNowSaoPaulo(date: Date | string, options?: { addSuffix?: boolean }) {
@@ -14,10 +14,5 @@ export function formatDistanceToNowSaoPaulo(date: Date | string, options?: { add
   const spTime = toZonedTime(dateObj, saoPaulo)
   return formatDistanceToNow(spTime, { ...options, locale: ptBR })
 }
-
-export function formatSaoPauloTime(date: Date | string, formatStr: string) {
-  const saoPaulo = 'America/Sao_Paulo'
-  const dateObj = typeof date === 'string' ? new Date(date) : date
-  const spTime = toZonedTime(dateObj, saoPaulo)
-  return format(spTime, formatStr, { locale: ptBR })
+  return formatDistanceToNow(spTime, { ...options, locale: ptBR })
 }
